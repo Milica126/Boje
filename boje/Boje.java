54400b1580cd42935037305c21a9d366ed81def0
Milica126 <milica.anticevic@gmail.com>
2025-11-28 14:12:43 +0100

    Prvi commit

---
+++ boje/Boje.java
@@ -0,0 +1,43 @@
+package boje;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Boje {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Boje window = new Boje();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Boje() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Boje {
+	requires java.desktop;
+}
